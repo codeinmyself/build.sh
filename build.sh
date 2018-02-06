d350b4ed0fdf02dcee6aa66182a7b6bf1ced5bdc
codeinmyself <1344863189@qq.com>
2018-02-06 09:47:13 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,29 @@
+# build.sh
+#在Linux下编译FFmpeg成功的脚本
+
+#!/bin/sh
+cd ffmpeg
+make clean
+export NDK=/home/lxq/userLee/android-ndk-r14b
+export PREBUILT=$NDK/toolchains/arm-linux-androideabi-4.9/prebuilt
+export PLATFORM=$NDK/platforms/android-9/arch-arm
+export PREFIX=../fflib
+build_one(){
+./configure --target-os=linux --prefix=$PREFIX \
+--enable-cross-compile \
+--enable-runtime-cpudetect \
+--disable-asm \
+--arch=arm \
+--cc=$PREBUILT/linux-x86_64/bin/arm-linux-androideabi-gcc \
+--cross-prefix=$PREBUILT/linux-x86_64/bin/arm-linux-androideabi- \
+--disable-stripping \
+--nm=$PREBUILT/linux-x86_64/bin/arm-linux-androideabi-nm \
+--sysroot=$PLATFORM \
+--enable-gpl --enable-shared --disable-static --enable-small \
+--disable-ffprobe --disable-ffplay --disable-ffmpeg --disable-ffserver --disable-debug \
+--extra-cflags="-fPIC -DANDROID -D__thumb__ -mthumb -Wfatal-errors -Wno-deprecated -mfloat-abi=softfp -marm -march=armv7-a"
+}
+build_one
+make
+make install
+cd ..
